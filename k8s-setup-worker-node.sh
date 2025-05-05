df66fe8714a83eb93be90fc9947ed78d8e15db78
planb438 <lilchris438@gmail.com>
2025-05-05 18:55:47 -0400

    Update k8s-setup-worker-node.sh

--- k8s-setup-worker-node.sh
+++ k8s-setup-worker-node.sh
@@ -1,3 +1,63 @@
 sudo -i
 
 apt-get update && apt-get upgrade -y
+
+apt install apt-transport-https \
+software-properties-common  ca-certificates tree socat -y
+
+swapoff -a
+
+---
+
+modprobe overlay
+modprobe br_netfilter
+
+
+cat << EOF | tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+
+sysctl --system
+
+---
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+| sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+root@worker: ̃# echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+---
+
+apt-get update &&  apt-get install containerd.io -y
+containerd config default | tee /etc/containerd/config.toml
+sed -e' s/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+systemctl restart containerd
+
+
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key \
+| sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" \
+| sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+---
+
+
+apt-get update
+
+
+apt-get install -y kubeadm=1.31.1-1.1 kubelet=1.31.1-1.1 kubectl=1.31.1-1.1
+
+apt-mark hold kubelet kubeadm kubectl
